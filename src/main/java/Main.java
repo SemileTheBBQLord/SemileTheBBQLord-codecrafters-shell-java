281c044dc426218430a8b691548d4f2cd6a64723
Emilio Navarro <equilibrium7890@gmail.com>
2025-05-05 22:53:55 -0500

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         
@@ -12,6 +11,7 @@ public class Main {
         
         int j = r.nextInt(1000);
         for (int i = 0;i < j;i++){
+        System.out.print("$ ");
         String input = scanner.nextLine();
         System.out.println(input + ": command not found");
         }
