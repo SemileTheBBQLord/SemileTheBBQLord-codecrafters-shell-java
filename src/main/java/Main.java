ae09b70b2385908321ae144949a6af0c046a576a
Emilio Navarro <equilibrium7890@gmail.com>
2025-05-09 23:38:34 -0500

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,9 +34,9 @@ public class Main {
           else {
           for (int n = 0; n < path_command.length; n++) {
             File file = new File(path_command[n], input2.trim());
-            if (file.exists()) {
-             System.out.println(input2.trim() + " is " + file.getAbsolutePath());
-            }
+
+            System.out.println(input2.trim() + " is " + file.getAbsolutePath());
+           
            }
           }
         }
