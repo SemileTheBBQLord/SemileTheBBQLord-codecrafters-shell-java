57fcec6ed3e93c1e436a67ae51a4fa2afc8917f2
Emilio Navarro <equilibrium7890@gmail.com>
2025-05-15 17:02:16 -0500

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,48 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Random;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-
-        Scanner scanner = new Scanner(System.in);
-        /*
-        String path_commands = System.getenv("PATH");
-        String[] path_command = path_commands.split(";");
-        */
-        String[] paths = System.getenv("PATH").split(";");
-        Random r = new Random();
-        String input, input2;
-        int status;
-        int j = r.nextInt(1000);
-        
-        for (int i = 0;i < j;i++){
+  public static void main(String[] args) throws Exception {
+    String[] commands = {"exit", "echo", "type"};
+    String[] paths = System.getenv("PATH").split(":");
+    try (Scanner scanner = new Scanner(System.in)) {
+      boolean isRunning = true;
+      while (isRunning) {
         System.out.print("$ ");
-        input = scanner.next();
-        
-        if (input.equalsIgnoreCase("exit")) {
-          status = scanner.nextInt();
-          System.exit(status);
-        }
-        else if (input.equalsIgnoreCase("echo")) {
-          input2 = scanner.nextLine();
-          System.out.println(input2.trim());
-        }
-        else if(input.equalsIgnoreCase("type")) {
-          input2 = scanner.nextLine();
-          input2 = input2.trim();
-          if (input2.equalsIgnoreCase("exit") || input2.equalsIgnoreCase("echo") || input2.equalsIgnoreCase("type")){
-          System.out.println(input2 + " is a shell builtin");
-          }
-           else {
+        String input = scanner.nextLine();
+        String command = input.split(" ")[0];
+        String rest = input.split(" ").length > 1 ? input.split(" ", 2)[1] : "";
+        switch (command) {
+        case "exit":
+          isRunning = false;
+          break;
+        case "echo":
+          System.out.println(rest);
+          break;
+        case "type":
+          if (Arrays.asList(commands).contains(rest))
+            System.out.println(rest + " is a shell builtin");
+          else {
             boolean foundFile = false;
             for (String path : paths) {
               // Check if the file exists in the path and is executable
-              if (new File(path + "/" + input2).exists() &&
-                  new File(path + "/" + input2).canExecute()) {
-                System.out.println(input2 + " is " + path + "/" + input2);
+              if (new File(path + "/" + rest).exists() &&
+                  new File(path + "/" + rest).canExecute()) {
+                System.out.println(rest + " is " + path + "/" + rest);
                 foundFile = true;
                 break;
               }
             }
             if (!foundFile)
-              System.out.println(input2 + ": not found");
-          }          
-          /*for (int n = 0; n < path_command.length; n++) {
-            File file = new File(path_command[n], input2);
-            if (file.exists()) {
-            System.out.println(input2 + " is " + file.getPath());
-            }
-           }
-           */
+              System.out.println(rest + ": not found");
+          }
+          break;
+        default:
+          System.out.println(input + ": command not found");
+          break;
         }
-        else {
-        System.out.println(input + ": command not found");
-        }
-        
-        }
-    
+      }
+      System.exit(0);
     }
+  }
 }
